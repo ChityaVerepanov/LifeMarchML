d950a7594623f96960525596981d19f1293c2184
viktorcerepanov <cherepanov0077@gmail.com>
2025-06-12 20:38:17 +0500

    minor fix6

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { provideEventPlugins } from "@taiga-ui/event-plugins";
 import { provideAnimations } from "@angular/platform-browser/animations";
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import {provideRouter, withHashLocation} from '@angular/router';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
 
 import { routes } from './app.routes';
-import {TUI_LANGUAGE, TUI_RUSSIAN_LANGUAGE} from '@taiga-ui/i18n';
-import {of} from 'rxjs';
-import {provideHttpClient} from '@angular/common/http';
-import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import { TUI_LANGUAGE, TUI_RUSSIAN_LANGUAGE } from '@taiga-ui/i18n';
+import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+// Регистрируем русскую локаль
+registerLocaleData(localeRu);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,5 +29,9 @@ export const appConfig: ApplicationConfig = {
       provide: LocationStrategy,
       useClass: HashLocationStrategy
     },
+    {
+      provide: LOCALE_ID,
+      useValue: 'ru-RU'
+    }
   ]
 };
